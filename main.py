a821b74e01700397efd5d354a4a5c8b399bbb72a
c0llision <0xmw@protonmail.com>
2018-01-29 15:39:15 +0000

    init

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python3
-import subprocess, platform
+import subprocess
+import platform
 
 
 HOSTS = [
-    ['desktop','192.168.2.2'],
+    ['pi', '192.168.192.27'],
+    ['desktop', '192.168.2.2'],
 ]
+
+
 def pingOk(sHost):
     try:
-        output = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower()=="windows" else 'c', sHost), shell=True)
-
+        subprocess.check_output("ping -{} 1 {}".format(
+            'n' if platform.system().lower() == "windows" else 'c', sHost),
+            shell=True)
     except Exception as e:
         return False
 
     return True
 
 # figure out longest name and ip
-name_len = 6
-ip_len = 0
+# name_len = 6
+# ip_len = 0
+# for name, ip in HOSTS:
+#     if len(name) > name_len:
+#         name_len = len(name)
+#     if len(ip) > ip_len:
+#         ip_len = len(ip)
+# print('-' * name_len)
+# print(' ' * round((name_len - 4)/2) + 'name')
+
+
+print("\n| Name           | IP              | Status")
+print("-------------------------------------------")
 for name, ip in HOSTS:
-    if len(name) > name_len:
-        name_len = len(name)
-    if len(ip) > ip_len:
-        ip_len = len(ip)
-print('-' * name_len)
-print(' ' * round((name_len - 4)/2) + 'name')
-for name, ip in HOSTS:
-    print(name)
-    print(pingOk(ip))
+    print('| %-15s| %-16s| %6s' %
+          (name, ip, 'Online' if pingOk(ip) else 'Offline'))
