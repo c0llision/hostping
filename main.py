991ec27551e196a23a72c872df7aeba4890c8c7c
c0llision <0xmw@protonmail.com>
2018-01-29 18:10:47 +0000

    test commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+'''hostping'''
 import subprocess
 import platform
 
@@ -9,7 +10,7 @@ HOSTS = [
 ]
 
 
-def pingOk(sHost):
+def ping(sHost):
     try:
         subprocess.check_output("ping -{} 1 {}".format(
             'n' if platform.system().lower() == "windows" else 'c', sHost),
@@ -35,4 +36,4 @@ print("\n| Name           | IP              | Status")
 print("-------------------------------------------")
 for name, ip in HOSTS:
     print('| %-15s| %-16s| %6s' %
-          (name, ip, 'Online' if pingOk(ip) else 'Offline'))
+          (name, ip, 'Online' if ping(ip) else 'Offline'))
